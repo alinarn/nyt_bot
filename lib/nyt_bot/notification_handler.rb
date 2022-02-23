356ff3ef47471ae9867d79025d2e46891a4fbb26
Alina <alinarnautova@gmail.com>
2022-02-23 16:48:56 -0500

    check date before sending notification

--- lib/nyt_bot/notification_handler.rb
+++ lib/nyt_bot/notification_handler.rb
@@ -1,5 +1,6 @@
 require 'sidekiq'
 require 'sidekiq-scheduler'
+require 'date'
 require_relative 'bot'
 
 class NotificationHandler
@@ -7,6 +8,10 @@ class NotificationHandler
   
   def perform
     data = Redis.current.get('data')
-    Bot.send_notification(data)
+    date_match = data.match(/\d{2} .* \d{4}/).to_s
+    date_published = Date.parse(date_match)
+    if date_published == Date.today
+      Bot.send_notification(data)
+    end
   end
 end
